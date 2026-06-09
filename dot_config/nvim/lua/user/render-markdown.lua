15edd126beb4d65b422ecc847bf778da2b67d5a4
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-06-09 18:49:06 +0700

    feat(nvim): change markdown header colors

--- dot_config/nvim/lua/user/render-markdown.lua
+++ dot_config/nvim/lua/user/render-markdown.lua
@@ -17,3 +17,53 @@ rm.setup({
 		},
 	},
 })
+
+-- Color patches
+
+local function patch_hl(name, patch)
+	local ok, current = pcall(vim.api.nvim_get_hl, 0, {
+		name = name,
+		link = false,
+	})
+
+	if not ok then
+		current = {}
+	end
+
+	vim.api.nvim_set_hl(0, name, vim.tbl_extend("force", current, patch))
+end
+
+local function apply_markdown_highlights()
+	vim.api.nvim_set_hl(0, "RenderMarkdownBullet", {
+		link = "Comment",
+	})
+
+	local heading_links = {
+		[1] = "Title",
+		[2] = "Function",
+		[3] = "Type",
+		[4] = "Keyword",
+		[5] = "Constant",
+		[6] = "Comment",
+	}
+
+	for level, group in pairs(heading_links) do
+		vim.api.nvim_set_hl(0, "@markup.heading." .. level .. ".markdown", {
+			link = group,
+		})
+
+		vim.api.nvim_set_hl(0, "RenderMarkdownH" .. level, {
+			link = group,
+		})
+
+		vim.api.nvim_set_hl(0, "RenderMarkdownH" .. level .. "Bg", {
+			bg = "NONE",
+		})
+	end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = apply_markdown_highlights,
+})
+
+apply_markdown_highlights()
