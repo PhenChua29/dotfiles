9643bea668949f2bc3dca5a337bca10cedbad4e8
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-06-16 12:58:17 +0700

    fix(nvim): increase lazygit windows size

--- dot_config/nvim/lua/plugins/lazygit.lua
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -8,5 +8,9 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 		},
+		init = function()
+			vim.g.lazygit_floating_window_scaling_factor = 1.0
+			vim.g.lazygit_floating_window_winblend = 0
+		end,
 	},
 }
