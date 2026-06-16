889c739ca9ac1acb9069a437045a8058a48e7349
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2026-06-16 12:56:14 +0700

    fix(nvim): remove blocking keymap in terminal mode

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -2,10 +2,9 @@ return {
 	{
 		"akinsho/toggleterm.nvim",
 		cmd = { "ToggleTerm", "ToggleTermToggleAll" },
+		opts = {},
 		keys = {
 			{ "<leader>t", "<cmd>silent ToggleTerm direction=float<CR>", desc = "Toggle terminal" },
-			{ "<leader>t", "<cmd>ToggleTermToggleAll<CR>", mode = "t", desc = "Toggle terminals" },
 		},
-		opts = {},
 	},
 }
